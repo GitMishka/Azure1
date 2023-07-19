95591915a8d7301ef0741203482f2a2097fc65a1
Misha <mishagrigoryan@gmail.com>
2023-07-18 20:57:52 -0400

    reworked

--- login.py
+++ login.py
@@ -1,53 +1,42 @@
-from flask import Flask, render_template, redirect, url_for, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for, flash
+import psycopg2
+import config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Manonthemoon123@database-1.cueq5a3aruqx.us-east-2.rds.amazonaws.com/postgres'
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.secret_key = 'your secret key'
 
-# Initialize SQLAlchemy
-db = SQLAlchemy(app)
+def check_user(username, password):
+    conn = psycopg2.connect(
+        host=config.pg_host,
+        database=config.pg_database,
+        user=config.pg_user,
+        password=config.pg_password
+    )
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'")
+    user = cur.fetchone()
+    conn.close()
 
-# Flask Login
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.init_app(app)
-
-# SQLAlchemy User model
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-@app.route("/")
-@login_required
-def home():
-    return "Welcome!"
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        user = User.query.filter_by(username=request.form['username']).first()
-        if user and check_password_hash(user.password, request.form['password']):
-            login_user(user)
-            return redirect(url_for('home'))
-        else:
-            return 'Invalid username or password'
+    if user:
+        return True
     else:
-        return render_template('login.html')
+        return False
 
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return 'You are now logged out'
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if check_user(username, password):
+        flash('Logged in successfully!', 'success')
+        return redirect(url_for('home'))
+    else:
+        flash('Invalid credentials. Please try again.', 'error')
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    db.create_all()  # creates the tables
-    app.run(debug=True)
+    app
